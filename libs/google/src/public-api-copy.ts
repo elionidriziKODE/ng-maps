9694568ece4958dd45d9f05509a71a8a25eafa08
elionidriziKODE <elionidrizi@kodelabs.com>
2025-03-25 14:54:26 +0100

    init v2

---
+++ libs/google/src/public-api-copy.ts
@@ -0,0 +1,15 @@
+/*
+ * Public API Surface of google
+ */
+
+// export * from './lib/google-maps-api-wrapper';
+// export * from './lib/managers/marker.manager';
+// export * from './lib/managers/circle.manager';
+// export * from './lib/managers/polygon.manager';
+// export * from './lib/managers/polyline.manager';
+// export * from './lib/managers/rectangle.manager';
+// export * from './lib/managers/info-window.manager';
+// export * from './lib/google.component';
+export * from './lib/google.module';
+// export * from './lib/options';
+// export { isValidMarkerIcon } from './lib/util/is-valid-marker-icon';
